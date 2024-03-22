ae985a3abf522d53696a0ee24c9209a30ac43c22
manuelazotavio <o.manuela@aluno.ifsp.edu.br>
2024-03-22 17:16:55 -0300

    delete cors

--- src/server.js
+++ src/server.js
@@ -4,9 +4,17 @@ import {PORT, HOST} from './config.js'
 import userRouter from './routers/userRouter.js'
 import productRouter from './routers/productRouter.js'
 import logger from './middlewares/logger.js'
+import cors from 'cors'
 
 const app = express()
 
+app.use(cors())
+
+// app.use(cors({
+//   origin: ['http://localhost:3000', 'http://localhost:8081', ],
+//   methods: ['POST', 'GET', 'PUT', 'DELETE'],
+//   allowedHeaders: ['Content-Type', 'Authorization']
+// }))
 app.use(logger)
 app.use(express.json())
 
