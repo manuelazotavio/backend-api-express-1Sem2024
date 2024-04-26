a28d8f58b130ad0a10639524cb95475b8f3553b3
manuelazotavio <o.manuela@aluno.ifsp.edu.br>
2024-04-26 15:46:19 -0300

    add .js in userModel

--- src/controllers/auth/login.js
+++ src/controllers/auth/login.js
@@ -1,4 +1,4 @@
-import userModel from "../../models/userModel";
+import userModel from "../../models/userModel.js";
 import zodErrorFormat from "../../helpers/zodErrorFormat.js";
 import bcrypt from 'bcrypt'
 import jwt from 'jsonwebtoken'
